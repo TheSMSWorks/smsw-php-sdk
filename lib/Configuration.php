1b877436b13f6b4a593084b75856e50be22f3fe3
The SMS Works <30385093+TheSMSWorks@users.noreply.github.com>
2018-02-06 10:38:13 +0000

    Updated base URI

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -88,7 +88,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = 'https://api.thesmsworks.co.uk:8080/v1';
+    protected $host = 'https://api.thesmsworks.co.uk/v1';
 
     /**
      * Timeout (second) of the HTTP request, by default set to 0, no timeout
